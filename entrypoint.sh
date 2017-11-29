37b132812ccafe79711f6875c8ab7e08707f88a9
LukasdeBoer <LukasdeBoer@users.noreply.github.com>
2017-11-29 21:28:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python horepgd.py -s /xmltv.sock -tvh $TVH_HOST -tvh_username $TVH_USERNAME -tvh_password $TVH_PASSWORD -nr $NUM_DAYS
+python horepgd.py -s /epggrab/xmltv.sock -tvh $TVH_HOST -tvh_username $TVH_USERNAME -tvh_password $TVH_PASSWORD -nr $NUM_DAYS
